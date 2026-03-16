8ef265db75d98530c9dbeada9a0c7b5b5405f8ae
Tejeswar Naidu S <tejeswarnaidu9@gmail.com>
2026-03-16 22:38:49 +0530

    feat: add route for lowercase resume form path and create Vercel configuration file

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/ResumeForm" element={<ResumeForm />} />
+          <Route path="/resumeform" element={<ResumeForm />} />
         </Routes>
       </main>
     </div>
